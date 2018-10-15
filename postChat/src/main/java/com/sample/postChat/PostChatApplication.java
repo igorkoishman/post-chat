78b675819ad69c7e3fc1317454434ba61956d1ed
ikoishman <ikoishman@ebay.com>
2018-10-15 16:43:30 +0300

    First commit

---
+++ postChat/src/main/java/com/sample/postChat/PostChatApplication.java
@@ -0,0 +1,12 @@
+package com.sample.postChat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PostChatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PostChatApplication.class, args);
+	}
+}
---
+++ postChat/src/test/java/com/sample/postChat/PostChatApplicationTests.java
@@ -0,0 +1,16 @@
+package com.sample.postChat;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class PostChatApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
